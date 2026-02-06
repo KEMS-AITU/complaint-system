5b6b90d07d81a3f6ab082a807fecbdee150a70ec
Impala4c <mansuradan9@gmail.com>
2026-02-06 13:30:38 +0500

    db

--- backend/complaint_system/settings.py
+++ backend/complaint_system/settings.py
@@ -79,17 +79,9 @@ WSGI_APPLICATION = 'complaint_system.wsgi.application'
 RAILWAY_ENV = os.environ.get("RAILWAY_ENV", "development")
 
 
-if RAILWAY_ENV == "production":
-    DATABASES = {
-        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
 
 
 
